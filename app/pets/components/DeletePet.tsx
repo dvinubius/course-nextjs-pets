30d360edb48bf8c9708c8fb77c89b305811a08ce
dvinubius <dvinubiu.narmon@gmail.com>
2024-02-09 11:38:41 +0200

    add router refresh to delete

--- app/pets/components/DeletePet.tsx
+++ app/pets/components/DeletePet.tsx
@@ -1,14 +1,17 @@
 'use client'
 
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import { BsTrash } from 'react-icons/bs'
 
 function DeletePet({name}: {name: string}) {
+  const router = useRouter();
+
   const handleDelete = async (name: string) => {
     const resp = await fetch(`http://localhost:5000/api/v1/pet/${name}`, {
       method: 'DELETE'
     });
-    console.log(resp);
+    router.refresh();
   }
   return (
     <BsTrash className='text-2xl cursor-pointer' onClick={() => handleDelete(name)}/>
--- app/pets/components/DeletePetRouteHandler.tsx
+++ app/pets/components/DeletePetRouteHandler.tsx
@@ -1,14 +1,17 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { BsTrash } from 'react-icons/bs'
 
 function DeletePet({name}: {name: string}) {
+  const router = useRouter();
+
   const handleDelete = async (name: string) => {
     const resp = await fetch(`http://localhost:3000/api/pet/${name}`, {
       method: 'DELETE'
     });
-    console.log(resp);
+    router.refresh();
   }
   return (
     <BsTrash className='text-2xl cursor-pointer' onClick={() => handleDelete(name)}/>
